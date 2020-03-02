36655c1496cb1b3c137bfd79b064d630d2d644a7
Jason Whitehorn <jason@whitehorn.us>
2020-03-02 13:01:59 -0600

    add rmdir syscall

--- kern/sysfile.c
+++ kern/sysfile.c
@@ -349,6 +349,67 @@ int sys_mkdir(void) {
     return 0;
 }
 
+int sys_mkdir(void) {
+    struct inode *ip, *dp;
+    struct dirent de;
+    char name[DIRSIZ], *path;
+    uint off;
+
+    if(argstr(0, &path) < 0){
+        return -1;
+    }
+
+    begin_op();
+    if((dp = nameiparent(path, name)) == 0) {
+        end_op();
+        return -1;
+    }
+
+    ilock(dp);
+
+    // Cannot unlink "." or "..".
+    if(namecmp(name, ".") == 0 || namecmp(name, "..") == 0){
+        goto bad;
+    }
+
+    if((ip = dirlookup(dp, name, &off)) == 0){
+        goto bad;
+    }
+    ilock(ip);
+
+    if(ip->nlink < 1){
+        panic("rmdir: nlink < 1");
+    }
+
+    if(ip->type != T_DIR || !isdirempty(ip)) {
+        iunlockput(ip);
+        goto bad;
+    }
+
+    memset(&de, 0, sizeof(de));
+    if(writei(dp, (char*)&de, off, sizeof(de)) != sizeof(de)){
+        panic("rmdir: writei");
+    }
+
+    dp->nlink--;
+    iupdate(dp);
+
+    iunlockput(dp);
+
+    ip->nlink--;
+    iupdate(ip);
+    iunlockput(ip);
+
+    end_op();
+
+    return 0;
+
+bad:
+    iunlockput(dp);
+    end_op();
+    return -1;
+}
+
 int sys_mknod(void) {
     struct inode *ip;
     char *path;
