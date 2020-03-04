c275ce960acbd2e42948787c6192849672fdeafc
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-04 09:00:59 -0600

    move string.h to dedicate headers path

--- user/headers/string.h
+++ user/headers/string.h
@@ -1,14 +1,17 @@
 // ulib.c
-int stat(const char*, struct stat*);
+
 char* strcpy(char*, const char*);
 char* strlcpy(char *dst, const char *src, int size);
 char* strcat_s(char *dest, char *right, int max_len);
-void *memmove(void*, const void*, int);
 char* strchr(const char*, char c);
 int strcmp(const char*, const char*);
+uint strlen(const char*);
+
+// not sure where the following should live yet:
+int stat(const char*, struct stat*);
+void *memmove(void*, const void*, int);
 void printf(int, const char*, ...);
 char* gets(char*, int max);
-uint strlen(const char*);
 void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
