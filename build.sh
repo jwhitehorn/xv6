2f1e64ab7b40003b7d430df41f3a503ca456a38f
Jason Whitehorn <jason.whitehorn@gmail.com>
2020-03-04 18:47:54 -0600

    halt build on failure

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 cd ./kern/
 echo "Building kernel..."
-make kernel
+make kernel || exit 1
 cd ../user
 echo "Building userland..."
-make userland
+make userland || exit 1
 cd ../ #end where we started
